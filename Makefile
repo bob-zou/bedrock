703663871acb42fb0b8987d5c7de9a5f2acd5a0e
Bob Zou <zyp19901009@gmail.com>
2021-12-01 09:47:27 +0800

    v1.0.2

--- Makefile
+++ Makefile
@@ -1,11 +1,10 @@
 TARGET = bedrock
-VERSION := $(shell cat VERSION)
 
 all: $(TARGET)
 
 .PHONY: $(TARGET)
 $(TARGET): $(GOFILES)
-	packr2 build -ldflags "-X main.commit=`git rev-parse --short HEAD` -X main.version=$(VERSION)" -o $(TARGET)
+	packr2 build -ldflags "-s -w" -o $(TARGET)
 
 clean:
 	@rm -rf ./$(TARGET)
--- main.go
+++ main.go
@@ -7,17 +7,13 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var (
-	_version bool
-	version  = "<UNDEFINED>"
-	commit   = "<UNDEFINED>"
-)
+var _showVersion bool
 
 var RootCmd = &cobra.Command{
 	Use: "bedrock",
 	RunE: func(c *cobra.Command, args []string) error {
-		if _version {
-			fmt.Printf("bedrock version %s, build %s\n", version, commit)
+		if _showVersion {
+			fmt.Printf("bedrock version %s\n", version)
 			return nil
 		}
 		return c.Usage()
@@ -27,7 +23,7 @@ var RootCmd = &cobra.Command{
 func init() {
 	RootCmd.AddCommand(New)
 	RootCmd.AddCommand(Upgrade)
-	RootCmd.Flags().BoolVarP(&_version, "version", "v", false, "show version")
+	RootCmd.Flags().BoolVarP(&_showVersion, "version", "v", false, "show version")
 }
 
 func main() {
